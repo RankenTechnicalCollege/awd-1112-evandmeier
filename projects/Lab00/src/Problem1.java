05b631f112c1a8b94b99613f9ab9fec38ad7bf1c
Evan Meier <evan_meier@insideranken.org>
2020-01-07 15:03:30 -0600

    Lab00 #1-4

---
+++ projects/Lab00/src/Problem1.java
@@ -0,0 +1,7 @@
+public class Problem1 {
+
+    // #1 : Write a program that prints "Hello World" to the screen.
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ projects/Lab00/src/Problem4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Problem4 {
+
+    // #4 : Write a program that asks the user for a number n and prints the sum of the numbers 1 to n.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
