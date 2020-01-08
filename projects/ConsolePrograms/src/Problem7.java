f9c35fb8374202312b515ec056947a0e17505cf6
Evan Meier <evan_meier@insideranken.org>
2020-01-08 13:56:23 -0600

    Lab Problem #7

--- projects/ConsolePrograms/src/Problem7.java
+++ projects/ConsolePrograms/src/Problem7.java
@@ -4,8 +4,11 @@ public class Problem7 {
     public static void main(String[] args) {
         for (int i = 1; i <= 12; i++) {
             for (int j = 1; j <= 12; j++) {
-
+                // we use %4d because the max digit count in this case is 3
+                System.out.printf("%4d", i *j);
             }
+
+            System.out.println();
         }
     }
 }
