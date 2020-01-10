cee074062835b8b947da973b97baccbc90301ef4
Evan Meier <evan_meier@insideranken.org>
2020-01-10 13:20:12 -0600

    Lab Problem #19

--- projects/ConsolePrograms/src/Problem19.java
+++ projects/ConsolePrograms/src/Problem19.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem19 {
 
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<Integer>() {
+            {
+                add(1);
+                add(2);
+                add(3);
+            }
+        };
 
+        ArrayList<String> strings = new ArrayList<String>() {
+            {
+                add("a");
+                add("b");
+                add("c");
+            }
+        };
+
+        System.out.println(combine(integers, strings).toString());
+    }
+
+    /*
+        By writing this generically, we allow for much more versatility in calling this function.
+        Essentially what is going on here is that we are forcing the elements in 'listA' and 'listB'
+        to be of the type 'T', or children of the type 'T'.
+
+        See 'Bounded Type Parameters' in the GENERICS section of the Java Quick Syntax Reference.
+     */
+    public static <T, R extends T, S extends T> List<T> combine(List<R> listA, List<S> listB) {
+        List<T> newList = new ArrayList<T>();
+
+        for (R element : listA) {
+            newList.add(element);
+        }
+
+        for (S element : listB) {
+            newList.add(element);
+        }
+
+        return newList;
+    }
 }
