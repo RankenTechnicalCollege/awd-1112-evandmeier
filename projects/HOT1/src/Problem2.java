94140b0404fbf6dc0abf6bacdb404eff8662a41b
Evan Meier <evan_meier@insideranken.org>
2020-01-16 16:20:35 -0600

    HOT1 Problem #2

---
+++ projects/HOT1/src/Problem2.java
@@ -0,0 +1,20 @@
+public class Problem2 {
+
+    public static void main(String[] args) {
+        System.out.printf("The sum of the first 100 perfect squares is: %d", sumOfPerfectSquares(100));
+    }
+
+    /*
+        This function calculates and returns the sum of
+        the perfect squares of 1 .. n
+     */
+    public static int sumOfPerfectSquares(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            sum += Math.pow(i, 2);
+        }
+
+        return sum;
+    }
+}
